7c74759b46e3a3fdad6f41e36365846ceaa3bbee
Teddy <lrc@zhimei360.com>
2015-09-29 18:20:11 +0800

    Update Test.php
    test one plus two.

--- Test.php
+++ Test.php
@@ -2,8 +2,8 @@
 
 class Test extends PHPUnit_Framework_TestCase
 {
-	public function testOnePlusOne() {
-		$this->assertEquals(1+1,2);
+	public function testOnePlusTwo() {
+		$this->assertEquals(1+2,3);
   	}
 }
 
